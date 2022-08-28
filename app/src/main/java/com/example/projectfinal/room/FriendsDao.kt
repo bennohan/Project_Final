ffcbb37f9ed1a58e958545a858022379bcfa9b53
bennohan <bebenbarnet@gmail.com>
2022-08-28 11:11:25 +0700

    update

---
+++ app/src/main/java/com/example/projectfinal/room/FriendsDao.kt
@@ -0,0 +1,21 @@
+package com.example.projectfinal.room
+
+import androidx.room.*
+
+@Dao
+interface FriendsDao {
+    @Insert
+    suspend fun addFriend(friendData: FriendData)
+
+    @Update
+    suspend fun editFriend(friendData: FriendData)
+
+    @Delete
+    suspend fun deleteFriend(friendData: FriendData)
+
+    @Query("SELECT * FROM FriendData")
+    suspend fun getFriend(): List<FriendData>
+
+    @Query("SELECT * FROM FriendData WHERE name LIKE :searchQuery OR school LIKE :searchQuery OR github LIKE :searchQuery OR github LIKE :searchQuery")
+    fun getsearchDatabase(searchQuery: String): List<FriendData>
+}
